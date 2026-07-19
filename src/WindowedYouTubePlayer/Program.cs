92f357a72eea595145a8fba1f5b918dc70a051f8
Mohsen <Mohsen000069@gmail.com>
2026-07-19 19:06:02 +0100

    Add application entry point

---
+++ src/WindowedYouTubePlayer/Program.cs
@@ -0,0 +1,11 @@
+namespace WindowedYouTubePlayer;
+
+internal static class Program
+{
+    [STAThread]
+    private static void Main()
+    {
+        ApplicationConfiguration.Initialize();
+        Application.Run(new MainForm());
+    }
+}
